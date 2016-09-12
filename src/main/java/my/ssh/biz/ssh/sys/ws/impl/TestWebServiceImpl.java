bad5e620eb32a6a535d8be6be6644adb1c4f8808
javazzy <javazzy@163.com>
2016-09-12 22:13:58 +0800

    update

--- src/main/java/my/ssh/biz/ssh/sys/ws/impl/TestWebServiceImpl.java
+++ src/main/java/my/ssh/biz/ssh/sys/ws/impl/TestWebServiceImpl.java
@@ -1,11 +1,8 @@
 package my.ssh.biz.ssh.sys.ws.impl;
 
-import my.ssh.biz.ssh.sys.service.TestService;
 import my.ssh.biz.ssh.sys.ws.TestWebService;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import javax.jws.WebService;
 
 /**
@@ -14,16 +11,8 @@ import javax.jws.WebService;
 @Component
 @WebService(endpointInterface = "my.ssh.biz.ssh.sys.ws.TestWebService")
 public class TestWebServiceImpl implements TestWebService{
-
-    @Resource
-    private TestService testService;
-
     @Override
     public String hello(String name) {
         return "hello "+name;
-    }
-
-    public void setTestService(TestService testService) {
-        this.testService = testService;
     }
 }
