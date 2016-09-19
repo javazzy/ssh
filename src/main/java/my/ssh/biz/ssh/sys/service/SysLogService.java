537e0536c47a6d1b0f87d7f27ef74f9176c6b6ae
javazzy <javazzy@163.com>
2016-09-19 14:54:34 +0800

    update

---
+++ src/main/java/my/ssh/biz/ssh/sys/service/SysLogService.java
@@ -0,0 +1,11 @@
+package my.ssh.biz.ssh.sys.service;
+
+import my.ssh.biz.common.service.BaseService;
+import my.ssh.biz.ssh.sys.entity.SysLog;
+
+/**
+ *  鏈嶅姟鎺ュ彛锛? 系统 - 日志表
+ */
+public interface SysLogService extends BaseService<SysLog> {
+
+}
