e2635d7e30f1759343df5616c4e5f2844798d212
javazzy <javazzy@163.com>
2017-01-14 23:41:57 +0800

    update

--- src/main/java/my/ssh/initializer/config/RedisCacheConfig.java
+++ src/main/java/my/ssh/initializer/config/RedisCacheConfig.java
@@ -41,60 +41,60 @@ public class RedisCacheConfig extends CachingConfigurerSupport {
     public JedisPoolConfig jedisPoolConfig(){
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
         //最大空闲连接数, 默认8个
-        if (propertySource.containsProperty("redis.maxIdle"))
-            jedisPoolConfig.setMaxIdle(Integer.parseInt(propertySource.getProperty("redis.maxIdle").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.maxIdle"))
+            jedisPoolConfig.setMaxIdle(Integer.parseInt(propertySource.getProperty("spring.redis.pool.maxIdle").toString()));
 
         //最大连接数, 默认8个
-        if (propertySource.containsProperty("redis.maxTotal"))
-            jedisPoolConfig.setMaxTotal(Integer.parseInt(propertySource.getProperty("redis.maxTotal").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.maxTotal"))
+            jedisPoolConfig.setMaxTotal(Integer.parseInt(propertySource.getProperty("spring.redis.pool.maxTotal").toString()));
 
         //最小空闲连接数, 默认0
-        if (propertySource.containsProperty("redis.minIdle"))
-            jedisPoolConfig.setMaxIdle(Integer.parseInt(propertySource.getProperty("redis.minIdle").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.minIdle"))
+            jedisPoolConfig.setMaxIdle(Integer.parseInt(propertySource.getProperty("spring.redis.pool.minIdle").toString()));
 
         //获取连接时的最大等待毫秒数(如果设置为阻塞时BlockWhenExhausted),如果超时就抛异常, 小于零:阻塞不确定的时间,  默认-1
-        if (propertySource.containsProperty("redis.maxWaitMillis"))
-            jedisPoolConfig.setMaxWaitMillis(Long.parseLong(propertySource.getProperty("redis.maxWaitMillis").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.maxWaitMillis"))
+            jedisPoolConfig.setMaxWaitMillis(Long.parseLong(propertySource.getProperty("spring.redis.pool.maxWaitMillis").toString()));
 
         //连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
-        if (propertySource.containsProperty("redis.blockWhenExhausted"))
-            jedisPoolConfig.setBlockWhenExhausted(Boolean.parseBoolean(propertySource.getProperty("redis.blockWhenExhausted").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.blockWhenExhausted"))
+            jedisPoolConfig.setBlockWhenExhausted(Boolean.parseBoolean(propertySource.getProperty("spring.redis.pool.blockWhenExhausted").toString()));
 
         //设置的逐出策略类名, 默认DefaultEvictionPolicy(当连接超过最大空闲时间,或连接数超过最大空闲连接数)
-        if (propertySource.containsProperty("redis.evictionPolicyClassName"))
-            jedisPoolConfig.setEvictionPolicyClassName(propertySource.getProperty("redis.evictionPolicyClassName").toString());
+        if (propertySource.containsProperty("spring.redis.pool.evictionPolicyClassName"))
+            jedisPoolConfig.setEvictionPolicyClassName(propertySource.getProperty("spring.redis.pool.evictionPolicyClassName").toString());
 
         //逐出连接的最小空闲时间 默认1800000毫秒(30分钟)
-        if (propertySource.containsProperty("redis.minEvictableIdleTimeMillis"))
-            jedisPoolConfig.setMinEvictableIdleTimeMillis(Long.parseLong(propertySource.getProperty("redis.minEvictableIdleTimeMillis").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.minEvictableIdleTimeMillis"))
+            jedisPoolConfig.setMinEvictableIdleTimeMillis(Long.parseLong(propertySource.getProperty("spring.redis.pool.minEvictableIdleTimeMillis").toString()));
 
         //对象空闲多久后逐出, 当空闲时间>该值 且 空闲连接>最大空闲数 时直接逐出,不再根据MinEvictableIdleTimeMillis判断  (默认逐出策略)
-        if (propertySource.containsProperty("redis.softMinEvictableIdleTimeMillis"))
-            jedisPoolConfig.setSoftMinEvictableIdleTimeMillis(Long.parseLong(propertySource.getProperty("redis.softMinEvictableIdleTimeMillis").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.softMinEvictableIdleTimeMillis"))
+            jedisPoolConfig.setSoftMinEvictableIdleTimeMillis(Long.parseLong(propertySource.getProperty("spring.redis.pool.softMinEvictableIdleTimeMillis").toString()));
 
         //每次逐出检查时 逐出的最大数目 如果为负数就是 : 1/abs(n), 默认3
-        if (propertySource.containsProperty("redis.numTestsPerEvictionRun"))
-            jedisPoolConfig.setNumTestsPerEvictionRun(Integer.parseInt(propertySource.getProperty("redis.numTestsPerEvictionRun").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.numTestsPerEvictionRun"))
+            jedisPoolConfig.setNumTestsPerEvictionRun(Integer.parseInt(propertySource.getProperty("spring.redis.pool.numTestsPerEvictionRun").toString()));
 
         //是否启用pool的jmx管理功能, 默认true
-        if (propertySource.containsProperty("redis.jmxEnabled"))
-            jedisPoolConfig.setJmxEnabled(Boolean.parseBoolean(propertySource.getProperty("redis.jmxEnabled").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.jmxEnabled"))
+            jedisPoolConfig.setJmxEnabled(Boolean.parseBoolean(propertySource.getProperty("spring.redis.pool.jmxEnabled").toString()));
 
         //是否启用后进先出, 默认true
-        if (propertySource.containsProperty("redis.lifo"))
-            jedisPoolConfig.setLifo(Boolean.parseBoolean(propertySource.getProperty("redis.lifo").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.lifo"))
+            jedisPoolConfig.setLifo(Boolean.parseBoolean(propertySource.getProperty("spring.redis.pool.lifo").toString()));
 
         //在获取连接的时候检查有效性, 默认false
-        if (propertySource.containsProperty("redis.testOnBorrow"))
-            jedisPoolConfig.setTestOnBorrow(Boolean.parseBoolean(propertySource.getProperty("redis.testOnBorrow").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.testOnBorrow"))
+            jedisPoolConfig.setTestOnBorrow(Boolean.parseBoolean(propertySource.getProperty("spring.redis.pool.testOnBorrow").toString()));
 
         //在空闲时检查有效性, 默认false
-        if (propertySource.containsProperty("redis.testWhileIdle"))
-            jedisPoolConfig.setTestWhileIdle(Boolean.parseBoolean(propertySource.getProperty("redis.testWhileIdle").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.testWhileIdle"))
+            jedisPoolConfig.setTestWhileIdle(Boolean.parseBoolean(propertySource.getProperty("spring.redis.pool.testWhileIdle").toString()));
 
         //逐出扫描的时间间隔(毫秒) 如果为负数,则不运行逐出线程, 默认-1
-        if (propertySource.containsProperty("redis.timeBetweenEvictionRunsMillis"))
-            jedisPoolConfig.setTimeBetweenEvictionRunsMillis(Integer.parseInt(propertySource.getProperty("redis.timeBetweenEvictionRunsMillis").toString()));
+        if (propertySource.containsProperty("spring.redis.pool.timeBetweenEvictionRunsMillis"))
+            jedisPoolConfig.setTimeBetweenEvictionRunsMillis(Integer.parseInt(propertySource.getProperty("spring.redis.pool.timeBetweenEvictionRunsMillis").toString()));
         return jedisPoolConfig;
     }
 
@@ -114,7 +114,7 @@ public class RedisCacheConfig extends CachingConfigurerSupport {
     @Bean
     public JedisConnectionFactory redisConnectionFactory() {
         JedisConnectionFactory redisConnectionFactory;
-        boolean redisClusterEnabled = Boolean.parseBoolean(propertySource.getProperty("redis.cluster.enabled").toString());
+        boolean redisClusterEnabled = Boolean.parseBoolean(propertySource.getProperty("spring.redis.cluster.enabled").toString());
 
         if(redisClusterEnabled) {
             redisConnectionFactory = new JedisConnectionFactory(redisClusterConfiguration(),jedisPoolConfig());
